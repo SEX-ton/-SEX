42a7a9a947e8cbe153b4c2addce5f340f4ba0fd3
dWinston <47751194+david0i@users.noreply.github.com>
2020-11-06 06:23:01 +0100

    Update sex.sol

--- contracts/sex.sol
+++ contracts/sex.sol
@@ -1,1 +1,127 @@
+pragma solidity 0.4.24;
 
+import "openzeppelin-eth/contracts/math/SafeMath.sol";
+import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+import "openzeppelin-eth/contracts/token/ERC20/ERC20Detailed.sol";
+
+import "./lib/SafeMathInt.sol";
+
+contract Sex is ERC20Detailed, Ownable {
+
+    using SafeMath for uint256;
+    using SafeMathInt for int256;
+
+    event LogRebase(uint256 indexed epoch, uint256 totalSupply);
+    event LogMonetaryPolicyUpdated(address monetaryPolicy);
+
+    // Used for authentication
+    address public monetaryPolicy;
+
+    modifier onlyMonetaryPolicy() {
+        require(msg.sender == monetaryPolicy);
+        _;
+    }
+
+    bool private rebasePausedDeprecated;
+    bool private tokenPausedDeprecated;
+
+    modifier validRecipient(address to) {
+        require(to != address(0x0));
+        require(to != address(this));
+        _;
+    }
+
+    uint256 private constant DECIMALS = 18;
+    uint256 private constant MAX_UINT256 = ~uint256(0);
+    uint256 private constant INITIAL_SUPPLY = 15000;
+
+    // TOTAL_GONS is a multiple of INITIAL_SUPPLY so that _gonsPerFragment is an integer.
+    // Use the highest value that fits in a uint256 for max granularity.
+    uint256 private constant TOTAL_GONS = MAX_UINT256 - (MAX_UINT256 % INITIAL_SUPPLY);
+
+    // MAX_SUPPLY = maximum integer < (sqrt(4*TOTAL_GONS + 1) - 1) / 2
+    uint256 private constant MAX_SUPPLY = ~uint128(0);  // (2^128) - 1
+
+    uint256 private _totalSupply;
+    uint256 private _gonsPerFragment;
+    mapping(address => uint256) private _gonBalances;
+
+    // This is denominated in Fragments, because the gons-fragments conversion might change before
+    // it's fully paid.
+    mapping (address => mapping (address => uint256)) private _allowedFragments;
+
+    /**
+     * @param monetaryPolicy_ The address of the monetary policy contract to use for authentication.
+     */
+    function setMonetaryPolicy(address monetaryPolicy_)
+        external
+        onlyOwner
+    {
+        monetaryPolicy = monetaryPolicy_;
+        emit LogMonetaryPolicyUpdated(monetaryPolicy_);
+    }
+    
+    function rebase(uint256 epoch, int256 supplyDelta)
+        external
+        onlyMonetaryPolicy
+        returns (uint256)
+    {
+        if (supplyDelta == 0) {
+            emit LogRebase(epoch, _totalSupply);
+            return _totalSupply;
+        }
+
+        if (supplyDelta < 0) {
+            _totalSupply = _totalSupply.sub(uint256(supplyDelta.abs()));
+        } else {
+            _totalSupply = _totalSupply.add(uint256(supplyDelta));
+        }
+
+        if (_totalSupply > MAX_SUPPLY) {
+            _totalSupply = MAX_SUPPLY;
+        }
+
+        emit LogRebase(epoch, _totalSupply);
+        return _totalSupply;
+    }
+
+    function initialize(address owner_)
+        public
+        initializer
+    {
+          ERC20Detailed.initialize("Sex Token", "SEX", uint8(DECIMALS));
+        Ownable.initialize(owner_);
+
+        rebasePausedDeprecated = false;
+        tokenPausedDeprecated = false;
+
+        _totalSupply = INITIAL_SUPPLY;
+        _gonBalances[owner_] = TOTAL_GONS;
+        _gonsPerFragment = TOTAL_GONS.div(_totalSupply);
+
+        emit Transfer(address(0x0), owner_, _totalSupply);
+    }
+
+    /**
+     * @return The total number of fragments.
+     */
+    function totalSupply()
+        public
+        view
+        returns (uint256)
+    {
+        return _totalSupply;
+    }
+
+    /**
+     * @param who The address to query.
+     * @return The balance of the specified address.
+     */
+    function balanceOf(address who)
+        public
+        view
+        returns (uint256)
+    {
+        return _gonBalances[who].div(_gonsPerFragment);
+    }
+}
